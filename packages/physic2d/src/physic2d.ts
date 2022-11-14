431594914cf9bd1a7a4b2c73ff530829ca128b06
thomas <destvincent@hotmail.fr>
2022-11-14 22:20:54 +0100

    refactor(physic2d): use type instead of boolean

--- packages/physic2d/src/physic2d.ts
+++ packages/physic2d/src/physic2d.ts
@@ -1,5 +1,9 @@
 export { default as ColliderCallback } from './components/ColliderCallback'
-export { default as Physic } from './components/Physic'
+export {
+  default as Physic,
+  PhysicParams,
+  PhysicType,
+} from './components/Physic'
 export {
   default as PhysicSystem,
   IGNORED_BY_WORLD,
