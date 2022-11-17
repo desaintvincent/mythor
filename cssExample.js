6fc3c603c9393422e6c552b4357e047336c4d707
desaintvincent <desaintvincent@users.noreply.github.com>
2022-11-17 14:01:49 +0000

    deploy: 0213b872d42158d89858e8d62fff1473316b3493

--- cssExample.js
+++ cssExample.js
@@ -1,12 +1,4 @@
 "use strict";
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 (self["webpackChunk_mythor_examples"] = self["webpackChunk_mythor_examples"] || []).push([["cssExample"],{
 
 /***/ "./src/templates/example.css":
@@ -15,7 +7,9 @@
   \***********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://@mythor/examples/./src/templates/example.css?");
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ })
 
@@ -25,3 +19,4 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/templates/example.css"));
 /******/ }
 ]);
+//# sourceMappingURL=cssExample.js.map
